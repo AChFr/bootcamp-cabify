82811d66871aa59f81cb85ee1ba040cd6826529f
AChFr <chirkovarseni@gmail.com>
2022-06-02 13:02:22 +0200

    added the -v flag to the executable.sh

--- executable.sh
+++ executable.sh
@@ -2,9 +2,9 @@
 echo "\n API ERROR TEST"
 echo "\n"
 echo "\n ALL IS CORRECT (may return a 'controlled 500')"
-curl -d "destination=testName" -d "message=testMessage" http://localhost:9001/messages
+curl -d "destination=testName" -d "message=testMessage" http://localhost:9001/messages -v
 echo "\n WRONG ENDPOINT"
-curl -d "destination=testName" -d "message=testMessage" http://localhost:9001/messagess
+curl -d "destination=testName" -d "message=testMessage" http://localhost:9001/messagess -v
 
 
 echo "\n"
@@ -12,18 +12,18 @@ echo "\n"
 echo "\n 9001 TEST"
 echo "\n"
 echo "\n INCOMPLETE PAYLOAD"
-curl  -d"destination=testName" http://localhost:9001/messages
+curl  -d"destination=testName" http://localhost:9001/messages -v
 echo "\n EXCESIVE PAYLOAD"
-curl  -d"destination=testName" -d "message=testMessage" -d "additionalKey=additionalValue" http://localhost:9001/messages
+curl  -d"destination=testName" -d "message=testMessage" -d "additionalKey=additionalValue" http://localhost:9001/messages -v
 echo "\n WRONG KEYS"
-curl  -d"errorDst=testName" -d "errorMsg=testMessage" http://localhost:9001/messages
+curl  -d"errorDst=testName" -d "errorMsg=testMessage" http://localhost:9001/messages -v
 echo "\n EMPTY VALUE: destination"
-curl  -d"destination=" -d "message=message" http://localhost:9001/messages
+curl  -d"destination=" -d "message=message" http://localhost:9001/messages -v
 echo "\n EMPTY VALUE: message"
-curl  -d"destination=testName" -d "message=" http://localhost:9001/messages
+curl  -d"destination=testName" -d "message=" http://localhost:9001/messages -v
 echo "\n WRONG METHOD: DELETE"
-curl -X DELETE http://localhost:9001/messages
+curl -X DELETE http://localhost:9001/messages -v
 echo "\n WRONG METHOD: PUT"
-curl -X PUT http://localhost:9001/messages
+curl -X PUT http://localhost:9001/messages -v
 echo "\n WRONG METHOD: PATCH"
-curl -X PATCH http://localhost:9001/messages
+curl -X PATCH http://localhost:9001/messages -v
